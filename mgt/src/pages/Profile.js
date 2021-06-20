94a038a3273ef313ff3ca19416fc29fb154de8ee
Imraj <imraj3@live.co.uk>
2021-06-20 16:18:17 +0100

    .

--- mgt/src/pages/Profile.js
+++ mgt/src/pages/Profile.js
@@ -1,8 +1,7 @@
-import {React} from 'react'
+import React, { useState, useRef } from "react";
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom';
-import { useSelector } from "react-redux";
-
+import { useDispatch, useSelector } from "react-redux";
 import { updatePassword } from "../actions/index";
 
 const Profile = () => {
